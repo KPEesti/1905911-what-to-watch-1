bf52f603602c271d73419629cea7225f47d81310
KPEesti <iwantoseeyou1@gmail.com>
2022-09-22 20:42:16 +0500

    Правка замечаний по предыдущим заданиям
    Выполнены правки по заданиям 1 и 4
    Добавлена ссылка на профиль куратора (задание 1)
    Задание 4: Файлы переименованы согласно критериям оценивания проектов.
    Исправлены замечания от eslint (одинарные кавычки и отступы)

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,5 @@
-import MainPage from "../../pages/MainPage";
-import MainPageProps from "./../../types/MainPageProps";
+import MainPage from '../../pages/MainPage/main-page';
+import MainPageProps from '../../types/main-page-props';
 
 function App({ name, genre, released }: MainPageProps): JSX.Element {
   return <MainPage name={name} genre={genre} released={released} />;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/app/app";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 const testProps = {
-  name: "The Grand Budapest Hotel",
-  genre: "Comedy",
+  name: 'The Grand Budapest Hotel',
+  genre: 'Comedy',
   released: 2014,
 };
 
