437de9c9ec2123b632c71f86a6dba1043e0d7706
KPEesti <iwantoseeyou1@gmail.com>
2022-11-10 12:51:00 +0500

    Rewrite header.tsx
    Add script for unauthorized user

--- project/src/components/Header/header.tsx
+++ project/src/components/Header/header.tsx
@@ -1,7 +1,11 @@
 import {Link} from 'react-router-dom';
+import {useAppSelector} from '../../hooks/store-hooks';
+import {AuthorizationStatus} from '../../utils/consts';
 
 
 export default function Header() {
+  const {authorizationStatus} = useAppSelector((state) => state);
+
   return (
     <header className="page-header film-card__head">
       <div className="logo">
@@ -12,21 +16,30 @@ export default function Header() {
         </Link>
       </div>
 
-      <ul className="user-block">
-        <li className="user-block__item">
-          <div className="user-block__avatar">
-            <img
-              src="img/avatar.jpg"
-              alt="User avatar"
-              width="63"
-              height="63"
-            />
+      {
+        authorizationStatus === AuthorizationStatus.Auth
+          ?
+          <ul className="user-block">
+            <li className="user-block__item">
+              <div className="user-block__avatar">
+                <img
+                  src="img/avatar.jpg"
+                  alt="User avatar"
+                  width="63"
+                  height="63"
+                />
+              </div>
+            </li>
+            <li className="user-block__item">
+              <Link className="user-block__link" to={'/login'}>Sign out</Link>
+            </li>
+          </ul>
+          :
+          <div className="user-block">
+            <Link className="user-block__link" to={'/login'}>Sign in</Link>
           </div>
-        </li>
-        <li className="user-block__item">
-          <Link className="user-block__link" to={'/login'}>Sign out</Link>
-        </li>
-      </ul>
+      }
+
     </header>
   );
 }
